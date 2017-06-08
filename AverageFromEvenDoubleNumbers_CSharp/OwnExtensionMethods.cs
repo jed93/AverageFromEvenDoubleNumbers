742cf3015ce6ef384258a1bda5c189c3863ce763
Jed93 <jedrekcebrat@gmail.com>
2017-06-08 19:17:00 +0200

    Zadanie 3

---
+++ AverageFromEvenDoubleNumbers_CSharp/OwnExtensionMethods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageFromEvenDoubleNumbers_CSharp
+{
+    public static class OwnExtensionMethods
+    {
+        public static double ReturnAverageFromEvenNumbers(this List<double> doubleList)
+        {
+            var evenNumbersList = new List<double>();
+              
+            foreach (var number in doubleList)
+            {
+                double unityNumber = Math.Floor(number);
+
+                // Sprawdzam podzielność przez 2
+                double numberDividedByTwo = unityNumber / 2;
+                if (numberDividedByTwo == Math.Round(numberDividedByTwo))
+                {
+                    evenNumbersList.Add(number);
+                }
+            }
+
+            double globalSum = 0;
+
+            foreach (var evenNumber in evenNumbersList)
+            {
+                globalSum += evenNumber;
+            }
+
+            return evenNumbersList.Count() > 0 ? globalSum / evenNumbersList.Count() : 0;
+        }
+    }
+}
---
+++ AverageFromEvenDoubleNumbers_CSharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AverageFromEvenDoubleNumbers_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<double> myList = new List<double> { 18.261, 32.81, 55.75, 543, 4.009, 33, 199.55, 2 };
+            double result = myList.ReturnAverageFromEvenNumbers();
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
